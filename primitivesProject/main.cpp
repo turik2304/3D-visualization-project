f2b6386cb8b71a9037fd707c56690380a7294a71
Artur <73557404+turik2304@users.noreply.github.com>
2021-01-24 18:38:57 +0300

    Update main.cpp

--- primitivesProject/main.cpp
+++ primitivesProject/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     setlocale(LC_NUMERIC,"C");
     // needed to ensure appropriate OpenGL context is created for VTK rendering.
     QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
-    // Create and start Lab#, #=0..3
+    // Create and start Lab#, # = 0..3
     Lab1 w;
     w.show();
     return a.exec();

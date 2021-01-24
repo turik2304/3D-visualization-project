cc2a22eb343a92a6ccf55962d2922c8eb70bb879
Artur <73557404+turik2304@users.noreply.github.com>
2021-01-24 20:14:34 +0500

    Add files via upload

---
+++ primitivesProject/lab1_Rocket.cpp
@@ -0,0 +1,131 @@
+/*
+//Подключение заголовочных файлов
+#include "widget.h"
+#include "ui_widget.h"
+
+//Основной конструктор класса
+Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget)
+{
+    ui->setupUi(this); //Установка виджета визуализации QVTKWidget
+//Основной алгоритм
+
+    //
+    //Создаём источник данных
+    //
+
+    vtkNew<vtkConeSource> head;
+    head->SetDirection(0, 1, 0);
+    head->SetHeight(3);
+    head->SetResolution(100);
+    head->SetRadius(1);
+
+    vtkNew<vtkCylinderSource> body;
+    body->SetHeight(14);
+    body->SetResolution(100);
+    body->SetRadius(1);
+
+    vtkNew<vtkConeSource> engine1;
+    engine1->SetDirection(0, 1, 0);
+    engine1->SetHeight(8);
+    engine1->SetResolution(100);
+    engine1->SetRadius(0.7);
+
+    vtkNew<vtkConeSource> engine2;
+    engine2->SetDirection(0, 1, 0);
+    engine2->SetHeight(8);
+    engine2->SetResolution(100);
+    engine2->SetRadius(0.7);
+
+    vtkNew<vtkConeSource> engine3;
+    engine3->SetDirection(0, 1, 0);
+    engine3->SetHeight(8);
+    engine3->SetResolution(100);
+    engine3->SetRadius(0.7);
+
+    vtkNew<vtkConeSource> engine4;
+    engine4->SetDirection(0, 1, 0);
+    engine4->SetHeight(8);
+    engine4->SetResolution(100);
+    engine4->SetRadius(0.7);
+    //
+    //Создаём актёров
+    //
+    vtkNew<vtkActor> headActor;
+    headActor->SetPosition(0, 8.5, 0);
+    headActor->GetProperty()->SetColor(1, 1, 1);
+
+    vtkNew<vtkActor> bodyActor;
+    bodyActor->GetProperty()->SetColor(1, 1, 1);
+
+    vtkNew<vtkActor> engine1Actor;
+    engine1Actor->SetPosition(1, -3.1, 0);
+    engine1Actor->GetProperty()->SetColor(0.6, 0.6, 0.6);
+    vtkNew<vtkActor> engine2Actor;
+    engine2Actor->SetPosition(-1, -3.1, 0);
+    engine2Actor->GetProperty()->SetColor(0.6, 0.6, 0.6);
+    vtkNew<vtkActor> engine3Actor;
+    engine3Actor->SetPosition(0, -3.1, 1);
+    engine3Actor->GetProperty()->SetColor(0.6, 0.6, 0.6);
+    vtkNew<vtkActor> engine4Actor;
+    engine4Actor->SetPosition(0, -3.1, -1);
+    engine4Actor->GetProperty()->SetColor(0.6, 0.6, 0.6);
+    //
+    //Создаём мэппер
+    //
+    vtkNew<vtkPolyDataMapper> headMapper;
+    headMapper->SetInputConnection( head->GetOutputPort() );
+
+    vtkNew<vtkPolyDataMapper> bodyMapper;
+    bodyMapper->SetInputConnection( body->GetOutputPort() );
+
+    vtkNew<vtkPolyDataMapper> engine1Mapper;
+    engine1Mapper->SetInputConnection( engine1->GetOutputPort() );
+    vtkNew<vtkPolyDataMapper> engine2Mapper;
+    engine2Mapper->SetInputConnection( engine1->GetOutputPort() );
+    vtkNew<vtkPolyDataMapper> engine3Mapper;
+    engine3Mapper->SetInputConnection( engine1->GetOutputPort() );
+    vtkNew<vtkPolyDataMapper> engine4Mapper;
+    engine4Mapper->SetInputConnection( engine1->GetOutputPort() );
+    //
+    //Add Mapper to Actor
+    //
+    headActor->SetMapper( headMapper );
+
+    bodyActor->SetMapper( bodyMapper );
+
+    engine1Actor->SetMapper( engine1Mapper );
+    engine2Actor->SetMapper( engine2Mapper );
+    engine3Actor->SetMapper( engine3Mapper );
+    engine4Actor->SetMapper( engine4Mapper );
+    //
+    //Add Actor to Renderer
+    //
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor( headActor );
+
+    renderer->AddActor( bodyActor );
+
+    renderer->AddActor( engine1Actor );
+    renderer->AddActor( engine2Actor );
+    renderer->AddActor( engine3Actor );
+    renderer->AddActor( engine4Actor );
+    //
+    //Add Renderer to RenderWindow
+    //
+    vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
+    renderWindow->AddRenderer( renderer );
+    //
+    //Add RenderWindow to QVTKWigdet
+    //
+
+    ui->qvtkWidget->SetRenderWindow( renderWindow );
+
+}
+
+//Деструктор класса
+Widget::~Widget()
+{
+    delete ui;
+}
+*/
+
---
+++ primitivesProject/lab2_SolarSystem.cpp
@@ -0,0 +1,143 @@
+/*
+//Подключение заголовочных файлов
+#include "widget.h"
+#include "ui_widget.h"
+
+//Основной конструктор класса
+Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget)
+{
+    ui->setupUi(this); //Установка виджета визуализации QVTKWidget
+//Основной алгоритм
+
+    //
+    //Создаём источник данных
+    //
+    vtkNew<vtkSphereSource> sun;
+    vtkNew<vtkSphereSource> mercury;
+    vtkNew<vtkSphereSource> venus;
+    vtkNew<vtkEarthSource> earth;
+    vtkNew<vtkSphereSource> mars;
+    vtkNew<vtkSphereSource> jupiter;
+    vtkNew<vtkSphereSource> saturn;
+    vtkNew<vtkDiskSource> ring;
+    vtkNew<vtkSphereSource> uranus;
+    vtkNew<vtkSphereSource> neptune;
+    vtkNew<vtkSphereSource> pluto;
+    sun->SetRadius(696.340);
+    mercury->SetRadius(2.439);
+    venus->SetRadius(6.051);
+    earth->SetRadius(6.371);
+    mars->SetRadius(3.389);
+    jupiter->SetRadius(69.911);
+
+    saturn->SetRadius(58.232);
+    ring->SetInnerRadius(92);
+    ring->SetOuterRadius(136.5);
+    ring->SetCircumferentialResolution(100);
+
+    uranus->SetRadius(25.362);
+    neptune->SetRadius(24.622);
+    pluto->SetRadius(1.188*10);
+
+    //
+    //Создаём актёров
+    //
+    vtkNew<vtkActor> sunActor;
+    vtkNew<vtkActor> mercuryActor;
+    vtkNew<vtkActor> venusActor;
+    vtkNew<vtkActor> earthActor;
+    vtkNew<vtkActor> marsActor;
+    vtkNew<vtkActor> jupiterActor;
+    vtkNew<vtkActor> saturnActor;
+    vtkNew<vtkActor> ringActor;
+    vtkNew<vtkActor> uranusActor;
+    vtkNew<vtkActor> neptuneActor;
+    vtkNew<vtkActor> plutoActor;
+    sunActor->SetPosition(-696.340, 0, 0);
+    mercuryActor->SetPosition(57.9, 0, 0);
+    venusActor->SetPosition(108.2, 0, 0);
+    earthActor->SetPosition(149.6, 0, 0);
+    marsActor->SetPosition(227.9, 0, 0);
+    jupiterActor->SetPosition(778.3, 0, 0);
+    saturnActor->SetPosition(1427, 0, 0);
+    ringActor->SetPosition(1427, 0, 0);
+    uranusActor->SetPosition(2869.6, 0, 0);
+    neptuneActor->SetPosition(4496.6, 0, 0);
+    plutoActor->SetPosition(5900, 0, 0);
+    //
+    //Создаём мэппер
+    //
+    vtkNew<vtkPolyDataMapper> sunMapper;
+    vtkNew<vtkPolyDataMapper> mercuryMapper;
+    vtkNew<vtkPolyDataMapper> venusMapper;
+    vtkNew<vtkPolyDataMapper> earthMapper;
+    vtkNew<vtkPolyDataMapper> marsMapper;
+    vtkNew<vtkPolyDataMapper> jupiterMapper;
+    vtkNew<vtkPolyDataMapper> saturnMapper;
+    vtkNew<vtkPolyDataMapper> ringMapper;
+    vtkNew<vtkPolyDataMapper> uranusMapper;
+    vtkNew<vtkPolyDataMapper> neptuneMapper;
+    vtkNew<vtkPolyDataMapper> plutoMapper;
+    sunMapper->SetInputConnection( sun->GetOutputPort() );
+    mercuryMapper->SetInputConnection( mercury->GetOutputPort() );
+    venusMapper->SetInputConnection( venus->GetOutputPort() );
+    earthMapper->SetInputConnection( earth->GetOutputPort() );
+    marsMapper->SetInputConnection( mars->GetOutputPort() );
+    jupiterMapper->SetInputConnection( jupiter->GetOutputPort() );
+    saturnMapper->SetInputConnection( saturn->GetOutputPort() );
+    ringMapper->SetInputConnection( ring->GetOutputPort() );
+    uranusMapper->SetInputConnection( uranus->GetOutputPort() );
+    neptuneMapper->SetInputConnection( neptune->GetOutputPort() );
+    plutoMapper->SetInputConnection( pluto->GetOutputPort() );
+
+    //
+    //Add Mapper to Actor
+    //
+    sunActor->SetMapper(sunMapper);
+    mercuryActor->SetMapper( mercuryMapper );
+    venusActor->SetMapper( venusMapper );
+    earthActor->SetMapper( earthMapper );
+    marsActor->SetMapper( marsMapper );
+    jupiterActor->SetMapper( jupiterMapper );
+    saturnActor->SetMapper( saturnMapper );
+    ringActor->SetMapper( ringMapper );
+    uranusActor->SetMapper( uranusMapper );
+    neptuneActor->SetMapper( neptuneMapper );
+    plutoActor->SetMapper( plutoMapper );
+
+    earthActor->GetProperty()->SetColor(0, 1, 0);
+    //
+    //Add Actor to Renderer
+    //
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor( sunActor );
+    renderer->AddActor( mercuryActor );
+    renderer->AddActor( venusActor );
+    renderer->AddActor( earthActor );
+    renderer->AddActor( marsActor );
+    renderer->AddActor( jupiterActor );
+    renderer->AddActor( saturnActor );
+    renderer->AddActor( ringActor );
+    renderer->AddActor( uranusActor );
+    renderer->AddActor( neptuneActor );
+    renderer->AddActor( plutoActor );
+
+    //
+    //Add Renderer to RenderWindow
+    //
+    vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
+    renderWindow->AddRenderer( renderer );
+    //
+    //Add RenderWindow to QVTKWigdet
+    //
+
+    ui->qvtkWidget->SetRenderWindow( renderWindow );
+
+}
+
+//Деструктор класса
+Widget::~Widget()
+{
+    delete ui;
+}
+*/
